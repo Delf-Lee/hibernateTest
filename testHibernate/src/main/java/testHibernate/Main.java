cc7f811b784c8b6753b664e54dd9a9c64e36a6ef
delf <ttjs2008@naver.com>
2017-04-22 17:03:42 +0900

    change Main class

--- testHibernate/src/main/java/testHibernate/Main.java
+++ testHibernate/src/main/java/testHibernate/Main.java
@@ -13,9 +13,54 @@ public class Main {
 	public static void main(String arg[]) {
 		SessionFactory = new Configuration().configure().buildSessionFactory();
 		// testCode1();
-		testCode2();
+		// testCode2();
+		testCode3();
 	}
-	
+
+	private static void testCode3() {
+		Book springBook = new Book();
+		springBook.setTitle("스프링 프로그래밍");
+
+		Book hibernateBook = new Book();
+		hibernateBook.setTitle("하이버네이트 프로그래밍");
+
+		Book htmlBook = new Book();
+		htmlBook.setTitle("HTML/CSS/JavaScript");
+
+		Author authorKim = new Author();
+		authorKim.setName("김남윤");
+
+		Author authorLee = new Author();
+		authorLee.setName("이재문");
+
+		Author authorHwang = new Author();
+		authorHwang.setName("황기태");
+
+		springBook.getAuthors().add(authorKim);
+		springBook.getAuthors().add(authorLee);
+		springBook.getAuthors().add(authorHwang);
+
+		hibernateBook.getAuthors().add(authorKim);
+		hibernateBook.getAuthors().add(authorLee);
+
+		htmlBook.getAuthors().add(authorKim);
+		htmlBook.getAuthors().add(authorHwang);
+
+		Session session = SessionFactory.openSession();
+		Transaction tx = session.beginTransaction();
+
+		session.save(springBook);
+		session.save(hibernateBook);
+		session.save(htmlBook);
+		
+//		session.save(authorHwang);
+//		session.save(authorKim);
+//		session.save(authorLee);
+
+		tx.commit();
+		session.close();
+	}
+
 	private static void testCode2() {
 		Person person1 = new Person();
 		person1.setFirstName("SangHoon");
@@ -24,9 +69,9 @@ public class Main {
 		license1.setLicenseNumber("1292032");
 		license1.setIssue_date(new Date());
 		license1.setPerson(person1);
-		
+
 		person1.setLicense(license1);
-		
+
 		Person person2 = new Person();
 		person2.setFirstName("DoYeon");
 		person2.setLastName("Park");
@@ -34,19 +79,18 @@ public class Main {
 		license2.setLicenseNumber("1492011");
 		license2.setIssue_date(new Date());
 		license2.setPerson(person2);
-		
+
 		person2.setLicense(license2);
-		
+
 		Session session = SessionFactory.openSession();
 		Transaction tx = session.beginTransaction();
-		
+
 		session.save(person1); // save parent table(class)
 		session.save(person2);
-		
+
 		tx.commit();
 		session.close();
 	}
-
 
 	private static void testCode1() {
 		Category category1 = new Category();
